4a98d45bd7e2f290b5019bcb1fe8b79504d0a79d
Akshay Pawar <46777220+PAWAR-AKSHAY@users.noreply.github.com>
2020-09-23 17:59:21 +0530

    Django rest captcha frontend in angular

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Captcha
+# Simple Captcha with Django Rest and Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.0.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup } from '@angular/forms';
+
+export interface CaptchaResponse {
+	captcha_key: string;
+	captcha_image: string;
+	image_type: string;
+	image_decode: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'captcha';
+  formData ={
+    name : '',
+    comment : '',
+    captcha_key : '',
+    captcha_value : '',
+  }
+
+  constructor(private http: HttpClient) { }
+
+  response : CaptchaResponse;
+
+  captchaSubmitted : boolean;
+  ngOnInit() {
+    this.http.post('http://127.0.0.1:8000/api/captcha/',{}).subscribe((next:CaptchaResponse) => {
+      this.response = next;
+      this.formData.captcha_key = next.captcha_key;
+    });
+  }
+
+  postData(form:FormGroup) {
+    console.log(form.value);
+    this.http.post('http://127.0.0.1:8000/api/home/comment',form.value).subscribe((next:{submitted:boolean}) => {
+      this.captchaSubmitted = next.submitted;
+    });
+  }
+
 }
